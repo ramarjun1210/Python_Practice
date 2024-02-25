797448e7d75033ed65f16f4c2627ff020c9c826d
SandeepPatil <sandeep4u.pune@gmail.com>
2024-02-25 21:39:27 +0530

    Comment Added In The Num.py

---
+++ Num.py
@@ -0,0 +1,13 @@
+"""
+Take Input From User and Pass It To Function to Calculate Square
+"""
+
+def cal_sqr(num):
+    return num ** 2
+
+num = int(input('Enter The Num = '))
+
+result = cal_sqr(num)
+
+#.format is used to print the Number and It's Square Off
+print('Square Of {} Is {}'.format(num,result))
