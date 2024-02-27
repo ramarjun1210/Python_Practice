67d7b760c959d0c57d55cf0d9d5103957837c2b0
SandeepPatil <sandeep4u.pune@gmail.com>
2024-02-27 21:54:59 +0530

    remove file

--- Mltplctn_Sum.py
+++ Mltplctn_Sum.py
@@ -3,3 +3,20 @@ Calculate the multiplication and sum of two numbers
 Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
 
 """
+
+def mltplctn (num1,num2):
+    return num1 * num2
+
+def sum_num(num1,num2):
+    return num1 + num2
+
+
+num1 = int(input("Enter Num1 = "))
+num2 = int(input('Enter Num2 = '))
+
+if (num1 * num2 <= 1000):
+    result= mltplctn(num1,num2)
+else:
+    result = sum_num(num1,num2)
+
+print(f'Result = {result}')
