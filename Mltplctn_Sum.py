c2f21add9e96775e167b1f59038f9edf4690e51a
SandeepPatil <sandeep4u.pune@gmail.com>
2024-02-27 21:23:51 +0530

    Calculate Multiplicaation-Sum

---
+++ Mltplctn_Sum.py
@@ -0,0 +1,5 @@
+"""
+Calculate the multiplication and sum of two numbers
+Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+"""
